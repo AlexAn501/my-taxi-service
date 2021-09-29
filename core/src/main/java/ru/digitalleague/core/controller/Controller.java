b7ea58c7e939d6b2cb6a9b8c03cbc5d092087ac2
AlexAn501 <Ximik501@gmail.com>
2021-09-29 08:35:48 +0300

    Fix Controller and RabbitListener

--- core/src/main/java/ru/digitalleague/core/controller/Controller.java
+++ core/src/main/java/ru/digitalleague/core/controller/Controller.java
@@ -2,54 +2,36 @@ package ru.digitalleague.core.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.digitalleague.core.model.OrderDetails;
 
 @RestController
+@Slf4j
 public class Controller {
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Autowired
-    RabbitTemplate template;
+    private AmqpTemplate amqpTemplate;
 
-    /**
-     * Принимает post-запрос, добавляет объект orderDetails в очередь order (myQueue1)
-     * @param orderDetails
-     */
-    @PostMapping("/orders")
-    public void addOrder(@RequestBody OrderDetails orderDetails) {
+    @PostMapping("/order-taxi")
+    public ResponseEntity<String> receive(@RequestBody OrderDetails orderDetails) {
+        log.info("Received message from postman" + orderDetails);
 
-        System.out.println("orderDetails.getCarModel() = " + orderDetails.getCarModel());
-        System.out.println("orderDetails.getLevel() = " + orderDetails.getLevel());
-        System.out.println("orderDetails.getClientNumber() = " + orderDetails.getClientNumber());
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String order = null;
+        String message = null;
         try {
-            order = objectMapper.writeValueAsString(orderDetails);
-            template.convertAndSend("order",order);
+            message = objectMapper.writeValueAsString(orderDetails);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
+        amqpTemplate.convertAndSend("order", message);
 
-        System.out.println("order = " + order);
-
+        return ResponseEntity.ok("Request to place order has been send");
     }
 }
-
-//В проект добавлены:
-//1. Несколько моделей.
-//2. Конфигурация для работы с RabbitMQ.
-//3. Маппер для работы с базой данных.
-//4. Миграция
-//Задание:
-//1. Создать контроллер, который будет получать
-// запрос на оказание услуги перевозки.
-//2. В контроллер должен передаваться JSON обьект,
-// данные из которого будут вставлены в обьект OrderDetails.
-//3. Информация о заказе должна быть передана в брокер сообщений.
-//4. Должен быть реализован слушатель очереди "order",
-// который получит информацию о заказе из брокера сообщений.
--- core/src/main/java/ru/digitalleague/core/listener/RabbitQueueListener.java
+++ core/src/main/java/ru/digitalleague/core/listener/RabbitQueueListener.java
@@ -1,18 +1,20 @@
 package ru.digitalleague.core.listener;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @EnableRabbit
 @Component
+@Slf4j
 public class RabbitQueueListener {
+
     /**
-     * Слушает очередь order Bean myQueue1
-     * @param message
+     * Получаем информацию о заказе.
      */
     @RabbitListener(queues = "order")
-    public void orderListener(String message){
-        System.out.println("message = " + message);
+    public void processRabbitMessage(String message) {
+        log.info("Received message from rabbitmq " + message);
     }
 }
