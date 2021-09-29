7d8ede30f03a394db53f7daafa7f9985396f7826
AlexAn501 <Ximik501@gmail.com>
2021-09-29 13:54:30 +0300

    added test and queries

--- core/src/main/java/ru/digitalleague/core/controller/Controller.java
+++ core/src/main/java/ru/digitalleague/core/controller/Controller.java
@@ -1,19 +1,23 @@
 package ru.digitalleague.core.controller;
 
+import lombok.extern.slf4j.Slf4j;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import ru.digitalleague.core.model.OrderDetails;
 
 @RestController
 @Slf4j
 public class Controller {
+
     @Autowired
     private ObjectMapper objectMapper;
 
--- core/src/main/java/ru/digitalleague/core/listener/RabbitMqListener.java
+++ core/src/main/java/ru/digitalleague/core/listener/RabbitMqListener.java
@@ -1,14 +1,18 @@
 package ru.digitalleague.core.listener;
 
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Component;
 
-@EnableRabbit
 @Component
+@EnableRabbit
 @Slf4j
-public class RabbitQueueListener {
+@ConditionalOnBean(value = RabbitTemplate.class)
+public class RabbitMqListener {
 
     /**
      * Получаем информацию о заказе.
